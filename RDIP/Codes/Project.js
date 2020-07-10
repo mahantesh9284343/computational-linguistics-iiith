bfd8463e585c0e8b0deeb2be818eca0f8055d20a
mahantesh9284343 <mahantesh9284343696@gmail.com>
2020-07-10 19:54:49 +0530

    Display the words of the sentences as clickable buttons

--- RDIP/Codes/Project.js
+++ RDIP/Codes/Project.js
@@ -8,6 +8,7 @@ var E7=["John goes to the library and studies","John studies and goes to the lib
 var E8=["John ate an apple so did she","she ate an apple so did John"]
 var E9=["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"]
 var E10=["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her"	,"yesterday I bought a book that I told her"]
+var correctE=["Mary and John went to church","John goes to the library and studies","some students like to study in the night","John ate an apple before afternoon","John went to church after eating","did he go to market","the woman who called my sister sells cosmetics","the teacher returned the book after she noticed the error","I told her that I bought a book yesterday","John ate an apple so did she"]
 
 
 var H1=["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"]
@@ -17,22 +18,62 @@ var H4=["राम खाकर सोया","खाकर राम सोय�
 var H5=["बिल्लियों को मारकर कुत्ता सो गया"	,"मारकर बिल्लियों को कुत्ता सो गया"	 ,"बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता"	,"कुत्ता सो गया बिल्लियों को मारकर"	,"कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"]
 var H6=["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"]
 var H7=["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब"," है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"] 
+var correctH=["राम और श्याम गयें बाजार","श्याम सोया और राम भी","मैंने उसे बताया कि राम सो रहा है","राम सोया खाकर","बिल्लियों को मारकर कुत्ता सो गया"	,"एक लाल किताब वहाँ है","एक बड़ी सी किताब वहाँ है	"]
 
 function selection()
 {
+	document.getElementById("disp1").innerHTML=""
+	count=0
+
 if(document.getElementById("english").selected)
 {
+
 	document.getElementById("para1").innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words"
-	document.getElementById("para2").innerHTML="(select the buttons in proper order)"
-}
-else if (document.getElementById("hindi").selected) 
-{
+    document.getElementById("para2").innerHTML="(select the buttons in proper order)";
+    /* Random function to select Random sentences */
+	var e=correctE[Math.floor(Math.random() * correctE.length)];
+	let body = document.getElementsByTagName("p")[2];
+    e1=e.split(" ");
+    /* Random function to jumble words in the sentences */
+	for(i=0;i<e1.length;i++)
+    {
+	    j=Math.floor(Math.random()*e1.length)
+	    e=e1[i];
+	    e1[i]=e1[j]
+	    e1[j]=e;
+    }
+    for(i=0;i<e1.length;i++)
+    {
+		let button = document.createElement("button");
+        button.innerHTML = e1[i]
+        body.appendChild(button);
+    }
+    }
+    else if (document.getElementById("hindi").selected) 
+    { 
 	document.getElementById("para1").innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words"
-	document.getElementById("para2").innerHTML="(select the buttons in proper order)"
+	document.getElementById("para2").innerHTML="(select the buttons in proper order)";
+	var h=correctH[Math.floor(Math.random() * correctH.length)];
+    h1=h.split(" ");
+	let body = document.getElementsByTagName("p")[2];
+	for(i=0;i<h1.length;i++)
+    {
+	    j=Math.floor(Math.random()*h1.length)
+	    e=h1[i];
+	    h1[i]=h1[j]
+	    h1[j]=e;
+    }
+    for(i=0;i<h1.length;i++)
+    {
+        let button = document.createElement("button");
+        button.innerHTML = h1[i]
+        body.appendChild(button)
+    }
+    }
+    else
+    {
+	    document.getElementById("para1").innerHTML=""
+	    document.getElementById("para2").innerHTML=""
+	    document.getElementById("disp1").innerHTML=""
+    }
 }
-else
-{
-	document.getElementById("para1").innerHTML=""
-	document.getElementById("para2").innerHTML=""
-}
-} 
