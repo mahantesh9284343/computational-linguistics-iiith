f6f31962ac0d3a7d779a995aed14b4e4ea6e7918
mahantesh9284343 <mahantesh9284343696@gmail.com>
2020-07-22 22:49:09 +0530

    Project3 Goal-03 Making the language and Sentence selection dropdown

--- RDIP_2/Codes/Project3.js
+++ RDIP_2/Codes/Project3.js
@@ -1,2 +1,19 @@
 var English = ["The child liked the chocolate.","She was stopped by the bravest knight.","Mary baked a cake for his birthday","She decorated the cake carefully","Mary wore a dress with polka dots."]
 var Hindi = ["राम ने सीता के लिए फल तोड़ा।","छोटे बच्चे पाठशाला जल्दी आयेंगे।","मेहनत का फल मीठा होता है।","वाह! वह खूबसूरत है।","पेड़ से पत्ते गिर गए।"]
+
+console.log(English)
+console.log(Hindi)
+
+function select1()
+{
+document.getElementById("engdrop").style.display="none"
+document.getElementById("hindrop").style.display="none"
+	if(document.getElementById("english").selected)
+	{
+      document.getElementById("engdrop").style.display="block"
+	}
+	else if(document.getElementById("hindi").selected)
+	{
+		  document.getElementById("hindrop").style.display="block"
+	}
+} 
