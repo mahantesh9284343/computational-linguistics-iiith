bbd9466366fbba090b47f0ce0b705830b7129c7b
mahantesh9284343 <mahantesh9284343696@gmail.com>
2020-07-22 23:36:29 +0530

    Project3 Goal-05 Different Choice dropdown in Table

--- RDIP_2/Codes/Project3.js
+++ RDIP_2/Codes/Project3.js
@@ -11,9 +11,7 @@ function select1()
     document.getElementById("disp").innerHTML=""
     document.getElementById("disp1").innerHTML=""
     document.getElementById("tab1").innerHTML=""
-    document.getElementById("but1").style.visibility="hidden"
-
-
+    // document.getElementById("but1").style.visibility="hidden"
 
     if(document.getElementById("english").selected)
     {
@@ -36,7 +34,7 @@ function select1()
 function select2()
 {   
     document.getElementById("disp1").innerHTML="<span style='font-style:italic;color:blue;'>Select the POS tag for corresponding words</span>"
-    document.getElementById("but1").style.visibility="visible"
+//    document.getElementById("but1").style.visibility="visible"
 	var table1=document.getElementsByTagName("table")[0]
 	table1.innerHTML=""
 	e=document.getElementById("engdrop1")
@@ -72,9 +70,37 @@ function select2()
             var td3 = document.createElement('td');
             var td4 = document.createElement('td');
             var text1 = document.createTextNode(ans1[i]);
-            var x = document.createTextNode(" ");
+            
             var text3 = document.createTextNode(" ");
             var text4 =document.createTextNode(" ");
+            var x = document.createElement("select");
+            var op1 = new Option();
+            var op2 = new Option();
+            var op3 = new Option();
+            var op4 = new Option();
+            var op5 = new Option();
+            var op6 = new Option();     
+            var op7 = new Option();
+            var op8 = new Option();
+            var op9 = new Option();
+            op1.text = "Noun";  
+            op2.text = "Pronoun";
+            op3.text = "Verb";
+            op4.text = "Adjective";
+            op5.text = "Adverb";
+            op6.text = "Determiner";
+            op7.text = "Preposition";
+            op8.text = "Conjunction";
+            op9.text = "Interjection";
+            x.options.add(op1);
+            x.options.add(op2);
+            x.options.add(op3);
+            x.options.add(op4);
+            x.options.add(op5);
+            x.options.add(op6);
+            x.options.add(op7);
+            x.options.add(op8);
+            x.options.add(op9);
             td1.appendChild(text1);
             td2.appendChild(x);
             td3.appendChild(text3);
@@ -92,14 +118,14 @@ function select2()
 	    document.getElementById("disp").innerHTML=""
         document.getElementById("disp1").innerHTML=""
         document.getElementById("tab1").innerHTML=""
-        document.getElementById("but1").style.visibility="hidden"
+        //document.getElementById("but1").style.visibility="hidden"
     }
 }
 
 function select3()
 {	
     document.getElementById("disp1").innerHTML="<span style='font-style:italic;color:blue;'>Select the POS tag for corresponding words</span>"
-    document.getElementById("but1").style.visibility="visible"
+    //document.getElementById("but1").style.visibility="visible"
 	var table1=document.getElementsByTagName("table")[0]
 	table1.innerHTML=""
 	e=document.getElementById("hindrop1")
@@ -135,7 +161,34 @@ function select3()
             var td3 = document.createElement('td');
             var td4 = document.createElement('td');
             var text1 = document.createTextNode(ans1[i]);
-            var x = document.createTextNode(" ");
+            var x = document.createElement("select");
+            var op1 = new Option();
+            var op2 = new Option();
+            var op3 = new Option();
+            var op4 = new Option();
+            var op5 = new Option();
+            var op6 = new Option();
+            var op7 = new Option();
+            var op8 = new Option();
+            var op9 = new Option();
+            op1.text = "Noun";
+            op2.text = "Pronoun";
+            op3.text = "Verb";
+            op4.text = "Adjective";
+            op5.text = "Adverb";
+            op6.text = "Determiner";
+            op7.text = "Postposition";
+            op8.text = "Conjunction";
+            op9.text = "Interjection";
+            x.options.add(op1);
+            x.options.add(op2);
+            x.options.add(op3);
+            x.options.add(op4);
+            x.options.add(op5);
+            x.options.add(op6);
+            x.options.add(op7);
+            x.options.add(op8);
+            x.options.add(op9);
             var text3 = document.createTextNode(" ");
             var text4 =document.createTextNode(" ");
             td1.appendChild(text1);
@@ -155,6 +208,6 @@ function select3()
 	    document.getElementById("disp").innerHTML=""
         document.getElementById("disp1").innerHTML=""
         document.getElementById("tab1").innerHTML=""
-        document.getElementById("but1").style.visibility="hidden"
+ //       document.getElementById("but1").style.visibility="hidden"
     }
 }
